f67357fa8f77fadd8eb07fe73832f394c0c8ff9f
Filippo Loddo <pippo@filicom.com>
2021-09-28 13:26:49 +0200

    Initial Tested version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # BTCLatestBlockExtract
+
+Reads block data from the Bitcoin API https://chain.api.btc.com/v3/block/latest/tx 
+and parses relevant fields in a json format as follows:
+    
+    {
+       "block_height":702457,
+       "block_time":"2021-09-27 17:55:03",
+       "fee":75317,
+       "inputs_value":8648968,
+       "outputs_value":8573651,
+       "addresses":[
+          "bc1q9hetmwa4n904gsz3wqfzc383cvv6umd9vl5e78",
+          "bc1qfunj9a3ewecu496lx23rnfwvs3y99v0ywtglsh35uz3v72ksp49sldzxgp"
+       ]
+    }
+
+
+
+The application checks at regular intervals if there is a new block info and if so it processes the data avoiding duplicate records. 
+
+The print statement is for demonstration purposes to show when the retrieved block is already fetched, this can be moved to a log in case of a production pipeline.
+    
+### Further Enhancements:    
+Currently the resulting extract is just printed for demonstration. 
+A Data sink can be created as a downstream system, for example as:
+* Insert the record into a Database/DWH
+* Send the record to a queue or messaging service for downstream applications
+* Append the records to a file
+
+Logging and error management
+* extra error management and network issues management.
+* General logging 
+    
---
+++ btcBlock.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 09:30:41 2021
+
+@author: Filippo Loddo
+"""
+
+import requests
+from datetime import datetime
+import time
+
+# Set the retrieval interval in seconds
+RETRIEVE_INTERVAL = 5
+# Get data from the API
+DATASOURCE = "https://chain.api.btc.com/v3/block/latest/tx"
+
+# Function to retrieve block data, updates the latest timestamp
+def get_blocks(latest_block_time):
+    try:
+        r = requests.get(DATASOURCE)
+        data = r.json()
+    except:
+        print('Could not retrieve API data')
+        return latest_block_time
+    # Get the blocks list
+    data_list = (data['data']['list'])
+    if not data_list:
+        print('list empty', data_list)
+        return latest_block_time
+    # Initialize block record and addresses list
+    block_info = {}
+    # Add data from every block:
+    for block in data_list:
+        addresses = []
+        block_info['block_height'] = block['block_height']
+        ts = block['block_time']
+        if ts > latest_block_time:
+            block_info['block_time'] = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            block_info['fee'] = block['fee']
+            block_info['inputs_value'] = block['inputs_value']
+            block_info['outputs_value'] = block['outputs_value']
+            outputs = block['outputs']
+            # Get all addresses
+            for output in outputs:
+                address = output['addresses'][0]
+                # add only valid addresses
+                if address:
+                    addresses.append(address)
+            block_info['addresses'] = addresses
+
+            print(block_info)
+        else:
+            print('no new block found')
+            return ts
+    return ts
+
+
+# Retrieve data every N seconds
+if __name__ == "__main__":
+    latest_time = 0
+    forever = True
+    while forever:
+        latest_time=get_blocks(latest_time)
+        time.sleep(RETRIEVE_INTERVAL)
